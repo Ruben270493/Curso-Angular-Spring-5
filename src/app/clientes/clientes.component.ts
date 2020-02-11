435832058afddb496b632c20203298a39fcc2ee8
Ruben <ruben270493@gmail.com>
2020-02-11 20:40:49 +0100

    Corrección de error

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,22 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes:Cliente[] = [
-    {id: 1, nombre: 'Rubén', apellido: 'Fernández de Castro', email: 'ruben@angular.es', createAt: '2020-02-11'},
-    {id: 2, nombre: 'Trang', apellido: 'Bui', email: 'trang@angular.es', createAt: '2020-02-11'},
-    {id: 3, nombre: 'Fernando', apellido: 'Fernández', email: 'fernando@angular.es', createAt: '2020-02-11'},
-    {id: 4, nombre: 'Laura', apellido: 'Fernández', email: 'laura@angular.es', createAt: '2020-02-11'},
-    {id: 5, nombre: 'Sergio', apellido: 'Gonzalez', email: 'sergio@angular.es', createAt: '2020-02-11'},
-    {id: 6, nombre: 'Juan', apellido: 'Villazan', email: 'juan@angular.es', createAt: '2020-02-11'},
-    {id: 7, nombre: 'Mariangeles', apellido: 'Perez', email: 'mariangeles@angular.es', createAt: '2020-02-11'},
-    {id: 8, nombre: 'Mari', apellido: 'Carril', email: 'mari@angular.es', createAt: '2020-02-11'},
-    {id: 9, nombre: 'Jose', apellido: 'Portugal', email: 'jose@angular.es', createAt: '2020-02-11'},
-    {id: 10, nombre: 'Laura', apellido: 'Cuba', email: 'laura@angular.es', createAt: '2020-02-11'},
-    {id: 11, nombre: 'Carlos', apellido: 'Lopez', email: 'carlos@angular.es', createAt: '2020-02-11'}
-  ];
+  public clientes:Cliente[];
+
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES:Cliente[] = [
+    {id: 1, nombre: 'Rubén', apellido: 'Fernández de Castro', email: 'ruben@angular.es', createAt: '2020-02-11'},
+    {id: 2, nombre: 'Trang', apellido: 'Bui', email: 'trang@angular.es', createAt: '2020-02-11'},
+    {id: 3, nombre: 'Fernando', apellido: 'Fernández', email: 'fernando@angular.es', createAt: '2020-02-11'},
+    {id: 4, nombre: 'Laura', apellido: 'Fernández', email: 'laura@angular.es', createAt: '2020-02-11'},
+    {id: 5, nombre: 'Sergio', apellido: 'Gonzalez', email: 'sergio@angular.es', createAt: '2020-02-11'},
+    {id: 6, nombre: 'Juan', apellido: 'Villazan', email: 'juan@angular.es', createAt: '2020-02-11'},
+    {id: 7, nombre: 'Mariangeles', apellido: 'Perez', email: 'mariangeles@angular.es', createAt: '2020-02-11'},
+    {id: 8, nombre: 'Mari', apellido: 'Carril', email: 'mari@angular.es', createAt: '2020-02-11'},
+    {id: 9, nombre: 'Jose', apellido: 'Portugal', email: 'jose@angular.es', createAt: '2020-02-11'},
+    {id: 10, nombre: 'Laura', apellido: 'Cuba', email: 'laura@angular.es', createAt: '2020-02-11'},
+    {id: 11, nombre: 'Carlos', apellido: 'Lopez', email: 'carlos@angular.es', createAt: '2020-02-11'},
+  ];
