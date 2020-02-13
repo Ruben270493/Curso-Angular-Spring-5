fc5c772bf2fa99ff2815eb1e75521175fda0fdcb
Ruben <ruben270493@gmail.com>
2020-02-13 20:12:52 +0100

    Llamada a la API Rest del Backend para listar los clientes.

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente.js';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+
+  constructor(private http:HttpClient) { }
 
   public getClientes():Observable<Cliente[]> {
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
 
 }
