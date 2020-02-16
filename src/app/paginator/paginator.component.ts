00485e21a59e980d29a0803c549c7477d1c71ec7
Ruben <ruben270493@gmail.com>
2020-02-16 12:13:54 +0100

    Rango de páginas en el paginador

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
 
   @Input()
   public paginador:any;
 
   public paginas:number[];
+  public desde:number;
+  public hasta:number;
 
   constructor() { }
 
-  ngOnInit() {
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((valor, indice) => indice + 1);
+  ngOnInit() {}
+
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    
+    this.desde = Math.min(Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
+
+    if (this.paginador.totalPages > 5) {
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((valor, indice) => indice + this.desde);
+    } else {
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((valor, indice) => indice + 1);
+    }
+
   }
 
 }
