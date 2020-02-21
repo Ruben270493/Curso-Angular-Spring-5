f40917a175a639061993281a3d02399c5bee28be
Ruben <ruben270493@gmail.com>
2020-02-21 16:24:24 +0100

    Opción por defecto en el select del formulario.

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -64,7 +64,12 @@ export class FormComponent implements OnInit {
   }
 
   public compararRegion(o1:Region, o2:Region):boolean {
+
+    if (o1 === undefined && o2 === undefined)
+      return true;
+      
     return o1 == null || o2 == null ? false : o1.id === o2.id;
+    
   }
 
 }
